97ab0334ec7401817b0df78511940a7c2a6ba71f
ManuKovaSG <kovacicmanu@gmail.com>
2023-06-30 14:27:47 +0200

    first commit

---
+++ FilmItem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorCRUD.model
+{
+    public class FilmItem
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string ReleaseDate { get; set; }
+
+        public string PosterUrl { get; set; }
+        public object Gender { get; set; }
+        public object Character { get; set; }
+        public object Gendre { get; set; }
+    }
+}
---
+++ FilmsService.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+
+namespace BlazorCRUD.model
+{
+    public class FilmsService : FilmsInterface
+    {
+        private readonly HttpClient _httpClient;
+        const string _baseUrl = "http://albertbdp.somee.com/api/peliculas";
+        const string _filmsEndpoint = "http://albertbdp.somee.com/api/peliculas";
+        const string _host = "albertbdp.somee.com";
+
+
+        public FilmsService(HttpClient httpClient) => _httpClient = httpClient;
+
+
+        public async Task<List<FilmItem>> GetFilms(List<FilmItem> filmItem)
+        {
+            ConfigureHttpClient();
+
+            var response = await _httpClient.GetAsync(_filmsEndpoint);
+            response.EnsureSuccessStatusCode();
+
+            using var stream = await response.Content.ReadAsStreamAsync();
+
+            FilmsDto? dto = await JsonSerializer.DeserializeAsync<FilmsDto>(stream);
+            return dto.data.Select(item => new FilmItem 
+            { 
+                Name = item.name,
+                Gendre = item.gendre.name,
+                Character = item.characters
+            }).ToList();
+
+        }
+
+        private void ConfigureHttpClient()
+        {
+            _httpClient.BaseAddress = new Uri(_baseUrl);
+            _httpClient.DefaultRequestHeaders.Add("Films", _host);
+        }
+
+        public Task<List<FilmItem>> GetFilms()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
